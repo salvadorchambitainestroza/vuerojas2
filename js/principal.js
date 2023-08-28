08c1aef67156790a5e39aabf5a2d1f6fdc11a650
salvadorchambitainestroza <110933237+salvadorchambitainestroza@users.noreply.github.com>
2023-08-27 20:13:33 -0600

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -1,1 +1,21 @@
+var app = new Vue ({
+    el: '#App',
+    data: {
+        nombre: '',
+        promedio: '',
+        lista: []
+    },
+    methods: {
+        agregarnota: function(){
+            if (this.nombre !== "" && this.promedio !== "") {
+                let estado = this.promedio >= 7 ? " Aprobado" : "Reprobado"; 
+                this.lista.push({nombre: this.nombre, promedio: this.promedio, estado: estado});
+                this.nombre = '';
+                this.promedio = '';
+            } else {
+                alert('El nombre y promedio son obligatorios');
+            }
+        }
+    }
+});
 
